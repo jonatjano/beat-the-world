a22f8a709066a8f65a7064a0c2cb8c67de722519
Jonathan <jonathan.selle@laposte.net>
2018-05-17 08:28:56 +0200

    Update varLetConst.js

--- src/examples/varLetConst.js
+++ src/examples/varLetConst.js
@@ -8,23 +8,49 @@
 // "undefined"
 console.log(aVar);
 
-// "Error aLet doesn't exist"
+// "Error: can't access lexical declaration `aLet' before initialization"
 // stop everything
 console.log(aLet);
 
-// "Error aConst doesn't exist"
+// "Error: can't access lexical declaration `aConst' before initialization"
 // stop everything
 console.log(aConst);
 
+var aVar = "var";
+let aLet = "let";
+const aConst = "const";
+
+// "var"
+console.log(aVar);
+
+// "let"
+console.log(aLet);
+
+// "const"
+console.log(aConst);
+
+
 // yes it is
 if (1 == "1") {
+    // "varA"
+    console.log(aVar);
+
+    // "Error: can't access lexical declaration `aLet' before initialization"
+    // stop everything
+    console.log(aLet);
+
+    // "Error: can't access lexical declaration `aConst' before initialization"
+    // stop everything
+    console.log(aConst);
+ 
     var aVar = "aVar";
     let aLet = "aLet";
     const aConst = "aConst";
 
     aVar = "varA";
     aLet = "letA";
-    // doesn't work const change value
+    // "Error: invalid assignment to const `aConst'"
+    // doesn't work const can't change value
     // stop everything
     aConst = "constA";
 
@@ -38,13 +64,26 @@ if (1 == "1") {
     console.log(aConst);
 }
 
+// they are not of the same type so this is true
+if(1 !== "1") {
+    // here they are available since you don't redefine them inside the if block
+    
+    // "let"
+    console.log(aLet);
+
+    // "const"
+    console.log(aConst);
+}
+
 // "varA"
 console.log(aVar);
 
-// "Error aLet doesn't exist"
-// stop everything
+// "let"
+// get back to value before if*
 console.log(aLet);
 
-// "Error aConst doesn't exist"
-// stop everything
+// "const"
+// get back to value before if*
 console.log(aConst);
+
+// *that's why var must be avoided unless your case specifically need it
